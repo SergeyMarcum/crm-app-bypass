bd3a4fc5f7405f63345974f536882665fe683bc5
SergeyMarcum <sergey.marcum@gmail.com>
2025-04-05 19:11:36 +0500

    add page users and fix code

--- src/features/user-list/components/EditButtonRenderer.tsx
+++ src/features/user-list/components/EditButtonRenderer.tsx
@@ -5,11 +5,21 @@ import { ICellRendererParams } from "ag-grid-community";
 
 export const EditButtonRenderer: React.FC<ICellRendererParams> = (props) => {
   const handleEdit = () => {
-    if (props.node.rowIndex !== null) {
-      props.api.startEditingCell({
-        rowIndex: props.node.rowIndex,
-        colKey: "role", // Начинаем редактирование с поля "Права доступа"
+    const { api, node } = props;
+    if (node.rowIndex !== null) {
+      const rowIndex: number = node.rowIndex; // Фиксируем rowIndex как number
+      api.startEditingCell({
+        rowIndex, // Используем зафиксированное значение
+        colKey: "role_id",
       });
+      setTimeout(() => {
+        api.startEditingCell({
+          rowIndex, // Здесь тоже используем зафиксированное значение
+          colKey: "status_id",
+        });
+      }, 0);
+    } else {
+      console.warn("Cannot start editing: rowIndex is null");
     }
   };
 
